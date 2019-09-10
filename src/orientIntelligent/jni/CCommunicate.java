417bc869dfdab0798b90eb725dd8149d0c3abefa
cvmbsdd <yzq__c@163.com>
2019-09-10 17:32:30 +0800

    20190910

--- src/orientIntelligent/jni/CCommunicate.java
+++ src/orientIntelligent/jni/CCommunicate.java
@@ -5,35 +5,32 @@ import orientIntelligent.jni.DFSLDataType.CS_A1;
 import orientIntelligent.jni.DFSLDataType.CS_A16;
 import orientIntelligent.jni.DFSLDataType.CS_addrField;
 
+import java.io.InputStream;
+
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 public class CCommunicate {
 
 
     static {
 
         // System.load("E:\\Idea\\JavaCallCPlus1\\src\\orientIntelligent\\dll\\DFSLProDemo.dll");
-        System.load("D:\\OrientIntelligent\\svn\\JavaCallCPlus\\src\\orientIntelligent\\dll\\DFSLProDemo.dll");
+        System.load("E:\\Idea\\JavaCallCPlus1\\src\\orientIntelligent\\dll\\DFSLProDemo.dll");
     }
     public void parse_msg_server(byte bytes[])
     {
-        boolean  ret_PW = true;
-        boolean ret_Tpv=true;
-        boolean ret_EC=true;
+        boolean ret_PW  = true;
+        boolean ret_Tpv = true;
+        boolean ret_EC  = true;
         CL1GetOpt tmpL1GetOpt = new CL1GetOpt();
         //step1 注册协议操作集
         int DFSLProID = tmpL1GetOpt.register_DFSLProOptS();
-       /* byte bytes[] = {0x68 ,0x14 ,0x00 ,0x14 ,0x00 ,0x68 ,0x59 ,(byte)0x86 ,(byte)0x80 ,0x03  ,(byte)0x99 ,0x00 ,0x00 ,0x00 ,0x02 ,(byte)0xF0 ,0x00 ,0x00 ,0x01 ,0x00
-                ,0x00 ,0x54 ,0x10 ,0x10 ,0x05 ,0x00 ,0x75 ,0x16};*/
-       //心跳帧
-        /*byte hearbeat[] = { 0x68 ,0x15 ,0x00 ,0x15 ,0x00 ,0x68 ,0x1B ,(byte)0x86 ,0x30 ,0x08  ,0x00 ,0x56 ,0x34 ,0x12 ,0x00 ,(byte)0xE0 ,0x00 ,0x00 ,0x04 ,0x00
-                ,0x02 ,0x00 ,0x39 ,0x30 ,0x10 ,0x09 ,0x00 ,(byte)0xFD ,0x16};*/
-//        byte hearbeat[] = {0x68 ,0x14 ,0x00 ,0x14 ,0x00 ,0x68 ,(byte)0xD9 ,(byte)0x86 ,0x30 ,0x08  ,0x00 ,0x56 ,0x34 ,0x12 ,0x02 ,0x70 ,0x00 ,0x00 ,0x04 ,0x00
-//                ,0x39 ,0x30 ,0x10 ,0x09 ,(byte)0x91 ,0x19 ,0x63 ,0x16};
-//       //登录帧
-//        byte login[] = {0x68 ,0x0E ,0x00 ,0x0E ,0x00 ,0x68 ,(byte)0xD9 ,(byte)0x86 ,0x30 ,0x08  ,0x00 ,0x56 ,0x34 ,0x12 ,0x02 ,0x70 ,0x00 ,0x00 ,0x01 ,0x00
-//                ,0x34 ,0x16};
-//
-//        byte logout[] = {0x68 ,0x0E ,0x00 ,0x0E ,0x00 ,0x68 ,(byte)0xD9 ,(byte)0x86 ,0x30 ,0x08  ,0x00 ,0x56 ,0x34 ,0x12 ,0x02 ,0x70 ,0x00 ,0x00 ,0x02 ,0x00
-//                ,0x35 ,0x16};
+
         //step2  检查数据包完整性
         Cenumclass.E_CheckPackage ret = tmpL1GetOpt.check_package(DFSLProID,bytes);
         if(ret != Cenumclass.E_CheckPackage.E_CKPKG_SUCCESS)
@@ -80,7 +77,7 @@ public class CCommunicate {
         CEvent event = tmpL1GetOpt.get_userData_dataUnit_events(DFSLProID);
         if(null == event)
         {
-            ret_EC=false;
+            ret_EC = false;
             System.out.println("未发现事件计数器");
         }
         else
@@ -166,9 +163,61 @@ public class CCommunicate {
 
         byte logout[] = {0x68 ,0x0E ,0x00 ,0x0E ,0x00 ,0x68 ,(byte)0xD9 ,(byte)0x86 ,0x30 ,0x08  ,0x00 ,0x56 ,0x34 ,0x12 ,0x02 ,0x70 ,0x00 ,0x00 ,0x02 ,0x00
                 ,0x35 ,0x16};
+        //socket通信
+        //step1 创建服务器套接字
+        //step2 等待连接accept
+        //step3 从socket读取数据
+        //step4 关闭socket
+        //step5 关闭服务器套接字
+        byte[] recvFram = null;
+        int lenRecvFram;
+        int port = 9998;
+        //ServerSocket server = new ServerSocket(port);
+        try {
+            //创建绑定到特定端口的服务器套接字  1-65535
+            ServerSocket serversocket = new ServerSocket(port);
+            while(true) {
+                //建立连接，获取socket对象
+                System.out.println("server将一直等待连接的到来");
+                Socket socket=serversocket.accept();
+
+                //消息提示框
+                JOptionPane.showMessageDialog(null, "有客户端连接到了本机9998端口哦");
+                //与客户端通信
+
+                // 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+                InputStream inputStream = socket.getInputStream();
+                byte[] bytes = new byte[1024];
+                int len;
+                if((len = inputStream.read(bytes)) > 0)
+                {
+                    recvFram = new byte[len];
+                    for (int i = 0; i < len; i++) {
+                        recvFram[i]=bytes[i];
+                    }
+                }
+//                StringBuilder sb = new StringBuilder();
+//                while ((len = inputStream.read(bytes)) != -1) {
+//                    //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+//                    sb.append(new String(bytes, 0, len,"UTF-8"));
+//
+//                    System.out.println("len:"+len);
+//                }
+//                System.out.println("get message from client: " + sb);
+                inputStream.close();
+                socket.close();
+                serversocket.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         CCommunicate com =  new CCommunicate();
-        com.parse_msg_server(hearbeat);
+        if(recvFram!=null)
+        com.parse_msg_server(recvFram);
     }
 
     public int parse_linkdt_dataUnit_server(CL1GetOpt tmpL1GetOpt,int DFSLProID)
